75cdfb3f6f84da12232d9b904ebefd4c9db811e6
ilya <ilya.cyepanov@gmail.com>
2023-09-20 22:15:02 +0200

    feat(api): Add celery task in api

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import hashlib
+import asyncio
+import json
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 import redis
-from detoxify import Detoxify
+
+from task import predict_toxicity
+
 
 app = FastAPI()
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
 
 @app.get('/predict/{message}')
 async def predict(message: str):
     message_hash = hashlib.sha256(message.encode()).hexdigest()
     redis_cashe = redis_client.get(message_hash)
     if redis_cashe:
-        return redis_cashe
+        return JSONResponse(content=json.loads(redis_cashe))
     else:
-        model = Detoxify('multilingual', device='cuda')
-        prediction = model.predict(message)
-        toxic_text = prediction[max(prediction)] > 0.5
-        for key in prediction:
-            prediction[key] = float(prediction[key])
-        return {'toxic_text': toxic_text, 'meta_data': prediction} 
+        task_result = predict_toxicity.apply_async(args=[message])
+
+        while not task_result.ready():
+            await asyncio.sleep(0.2)
+
+        if task_result.successful():
+            result = task_result.result
+            json_data = json.dumps(result)
+            redis_client.set(message_hash, json_data)
+            return JSONResponse(content=result)
+        else:
+            raise HTTPException(status_code=500, detail="Task failed")
