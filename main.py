bd310b6cb65618f009ce1d7edf9b0080c5a96595
ilya <ilya.cyepanov@gmail.com>
2023-09-20 21:00:39 +0200

    feat(api): Add redis cashe to API

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import hashlib
+
 from fastapi import FastAPI
+import redis
 
 app = FastAPI()
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get('/predict/{message}')
 async def predict(message: str):
+    message_hash = hashlib.sha256(message.encode()).hexdigest()
+    redis_cashe = redis_client.get(message_hash)
+    if redis_cashe:
+        return redis_cashe
+    else:
+        pass
     return 'out'
