5dacf4de1620d842814c39524d27221376602134
ilya <ilya.cyepanov@gmail.com>
2023-09-21 17:46:51 +0200

    docs: Add docsting

--- main.py
+++ main.py
@@ -11,11 +11,27 @@ from task import predict_toxicity
 
 
 app = FastAPI()
-redis_client = redis.Redis(host='redis', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0) # must use user and password in production
 
 
 @app.get('/predict/{message}')
 async def predict(message: str):
+    """
+    Predict toxicity of a given message.
+
+    This endpoint takes a message as input, calculates its SHA-256 hash, and checks if the prediction result is cached
+    in Redis. If it's cached, it returns the cached result; otherwise, it asynchronously calculates the toxicity using
+    the 'predict_toxicity' task and caches the result for future use.
+
+    Parameters:
+        message (str): The message for which toxicity is to be predicted.
+
+    Returns:
+        JSONResponse: A JSON response containing the toxicity prediction result.
+
+    Raises:
+        HTTPException: If the prediction task fails, it raises a 500 Internal Server Error.
+    """
     message_hash = hashlib.sha256(message.encode()).hexdigest()
     redis_cache = redis_client.get(message_hash)
     if redis_cache:
--- task.py
+++ task.py
@@ -4,13 +4,27 @@ from celery import Celery
 
 app = Celery(
     'celery_app',
-    broker='amqp://guest:guest@rabbitmq:5672/',
+    broker='amqp://guest:guest@rabbitmq:5672/', # must use another user and password in production
     backend='rpc://'
 )
 
 
 @app.task
 def predict_toxicity(text):
+    """
+    Predict toxicity of a given text.
+
+    This Celery task uses the 'Detoxify' library to predict the toxicity of the provided text. It loads a pre-trained
+    'multilingual' model on the GPU if available, predicts the toxicity, and returns the result.
+
+    Parameters:
+        text (str): The text for which toxicity is to be predicted.
+
+    Returns:
+        dict: A dictionary containing the toxicity prediction result and associated meta-values.
+            - 'toxic_text' (str): Either 'Toxic' or 'Nontoxic' based on the prediction threshold.
+            - 'meta_value' (dict): A dictionary with toxicity scores for various categories.
+    """
     model = Detoxify('multilingual', device='cuda')
     prediction = model.predict(text)
     toxic_text = 'Toxic' if prediction[max(prediction)] > 0.5 else 'Nontoxic'
